0f9e0aae2099f76d0c6d49c0e14c1baf60080f42
Ramesh <ramesh@panaceatek.com>
2016-11-25 17:43:15 +0530

    updated

--- web.js
+++ web.js
@@ -1,15 +1,34 @@
 "use strict";
 
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-// var path = require('path');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt-nodejs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var mongoose = require('mongoose');
 
-// configuration ===========================================
-
+var models_user = require('./server/models/user.js');
 // config files
 var db = require('./server/config/db');
+//connection database
+// mongoose.connect(config.db);
+
+//import the routers
+var router = require('./server/routes/routes'); // configure our routes
+var authenticate = require('./server/routes/auth')(passport);
+
+var app = express();
+//tell node that My application will use ejs engine for rendering, view engine setup
+app.set('views', path.join(__dirname, 'client/public/views'));
+// app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+// configuration ===========================================
 
 // set our port
 var port = process.env.PORT || 8080;
@@ -18,31 +37,35 @@ var port = process.env.PORT || 8080;
 // (uncomment after you enter in your own credentials in config/db.js)
 // mongoose.connect(db.url); 
 
+//tell node the global configuration about parser,logger and passport
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(session({
+    secret: 'keyboard cat'
+}));
 // get all data/stuff of the body (POST) parameters
-// parse application/json 
+// parse application/json
 app.use(bodyParser.json());
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize()); //initializing passport
+app.use(passport.session()); //initializing passport session
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/client/public'));
 
 // routes ==================================================
-require('./server/routes/routes')(app); // configure our routes
-require('./server/routes/auth')(app);
+app.use('/', router);
+app.use('/auth', authenticate);
+//providing auth-api to passport so that it can use it.
+var initPassport = require('./server/passport/passport-init');
+initPassport(passport);
 // start app ===============================================
-// startup our app at http://localhost:8080
-app.listen(port);
+//running server on node
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('app listening at http://%s:%s', host, port);
+});
 
-// shoutout to the user                     
-console.log('Magic happens on port ' + port);
 
 // expose app           
 exports = module.exports = app;                         
